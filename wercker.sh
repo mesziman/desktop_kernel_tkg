c0b2d1aaea39e3d7048e1a600465e9cc429f09e5
mesziman <meszaros.gabor.10k@gmail.com>
2020-12-21 10:01:47 +0100

    mytkg kernel

---
+++ wercker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dpkg --add-architecture amd64
+apt-get -qq update > /dev/null ;
+apt-get -qq install -y  dialog apt-utils > /dev/null ;
+apt-get remove -y clang;
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+apt-get -qq install -y git build-essential wget tar kernel-package fakeroot libncurses5-dev libssl-dev ccache bison flex qtbase5-dev
+export LOFASZ=$PWD;
+
+mkdir -p /pipeline/build/root/toolchain/gcc;
+cd /pipeline/build/root/toolchain/gcc;
+wget https://mirrors.edge.kernel.org/pub/tools/crosstool/files/bin/x86_64/10.1.0/x86_64-gcc-10.1.0-nolibc-x86_64-linux.tar.xz
+tar xfv
+cd $LOFASZ
+bash builder.sh
