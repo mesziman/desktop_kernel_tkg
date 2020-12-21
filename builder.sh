8c396c5625bdfee061475a8d325518ce18e3301c
mesziman <meszaros.gabor.10k@gmail.com>
2020-12-21 13:24:19 +0100

    build in wercker CI

--- builder.sh
+++ builder.sh
@@ -4,13 +4,12 @@ git submodule update scripts
 
 KERNEL_DIR=$PWD
 CCACHEDIR=../CCACHE/kernel
-TOOLCHAINDIR=/pipeline/build/root/toolchain/gcc/gcc-10.1.0-nolibc/x86_64-linux/bin
+TOOLCHAINDIR=/pipeline/build/root/toolchain/gcc/gcc-10.1.0-nolibc/x86_64-linux
 DATE=$(date +"%d%m%Y")
 VER=$(git rev-parse --short HEAD)
 FINAL_ZIP="tkg-notebook-"$VER"".zip
 corenumber=$( nproc --all )
 buildspeed=$(( $corenumber + 2 ))
-
 
 export PATH="/usr/lib/ccache/bin/:$PATH"
 export CCACHE_SLOPPINESS="file_macro,locale,time_macros"
@@ -19,11 +18,13 @@ export PATH="${TOOLCHAINDIR}/bin:${TOOLCHAINDIR}/lib:${TOOLCHAINDIR}/include:${P
 export KBUILD_BUILD_USER="mesziman"
 export KBUILD_BUILD_HOST="github"
 
-make -j ${buildspeed} deb-pkg LOCALVERSION=-"tkg-gcc" -s;
+
+PATH="${TOOLCHAINDIR}/bin:${TOOLCHAINDIR}/lib:${TOOLCHAINDIR}/include:${PATH}" \
+make -j ${buildspeed} deb-pkg CROSS_COMPILE=${TOOLCHAINDIR}/bin/x86_64-linux- LOCALVERSION=-"tkg-gcc" -s;
 
 echo "======================VERIFY CLANG==============================="
-cat ./out/include/generated/compile.h
+cat ./include/generated/compile.h
 echo "======================VERIFY CLANG==============================="
-zip -r9 $FINAL_ZIP *.deb
+zip -r9 $FINAL_ZIP ../*.deb
 cp $FINAL_ZIP ${WERCKER_REPORT_ARTIFACTS_DIR}/
 mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
